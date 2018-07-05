b6c563a39b63ccd7d4f73156bbe84a9af031ab80
Gabriel Lisboa <gabrielbento.rx@outlook.com>
2018-07-04 23:19:06 -0300

    FizzBuzz challenger

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const FizzBuzz = (number) => {
+    if (number % 5 === 0 && number % 3 === 0) return 'FizzBuzz';
+    if (number % 3 === 0) return 'Fizz';
+    if (number % 5 === 0) return 'Buzz';
+
+    return number;
+}
+
+export default FizzBuzz;
